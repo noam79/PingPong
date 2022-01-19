184bb3ca8f1fc8dfc99cdda596461d8add46d0f9
noam79 <noameilon8@gmail.com>
2022-01-19 17:52:54 +0200

    Added ClientFactory

---
+++ PingPongClient/ClientFactory.cs
@@ -0,0 +1,23 @@
+﻿using PingPongClient.ObjectProviders;
+using PingPongClient.Output;
+using PingPongClient.ServerContact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongClient
+{
+    public class ClientFactory
+    {
+        public Client Create()
+        {
+            var objectProvider = new ConsoleStringProvider();
+            var serverContact = new SocketContact();
+            var output = new ConsoleOutput();
+
+            return new Client(objectProvider, serverContact, output);
+        }
+    }
+}
