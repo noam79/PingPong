4fe6f1ca865754559c1680d6733754996fff7de1
noam79 <noameilon8@gmail.com>
2022-01-19 17:34:13 +0200

    Added ConsoleStringProvider

---
+++ PingPongClient/ObjectProviders/ConsoleStringProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongClient.ObjectProviders
+{
+    public class ConsoleStringProvider : IObjectProvider
+    {
+        public object GetObject()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
