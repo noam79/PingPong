f90ed3fb88e39c5dbc086d38a5b679b255403df7
noam79 <noameilon8@gmail.com>
2022-01-19 17:10:17 +0200

    Added server factory

---
+++ PingPongServer/SocketTcpServerFactory.cs
@@ -0,0 +1,25 @@
+﻿using PingPongServer.SocketListeners;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingPongServer.SocketClientHandlers;
+
+namespace PingPongServer
+{
+    public class SocketTcpServerFactory
+    {
+        public TcpServer Create()
+        {
+            var listener = new DefaultSocketListener(
+                int.Parse(ConfigurationManager.AppSettings["port"]), 
+                int.Parse(ConfigurationManager.AppSettings["maxClients"]));
+
+            var clientHandler = new PingPongSocketHandler();
+
+            return new TcpServer(listener, clientHandler);
+        }
+    }
+}
