e0c339af888d4cb499c85d0f22b7f633563ab421
noam79 <noameilon8@gmail.com>
2022-01-19 16:13:40 +0200

    Added ISocketClientHandler

---
+++ PingPongServer/SocketClientHandlers/ISocketClientHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer.SocketClientHandlers
+{
+    public interface ISocketClientHandler
+    {
+        public void HandleClient(Socket socket);
+    }
+}
