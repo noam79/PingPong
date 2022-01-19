8c2615bd969f7a2ad9bbe9d31b1c4813e7bf3298
noam79 <noameilon8@gmail.com>
2022-01-19 17:30:31 +0200

    Added output abstraction

---
+++ PingPongClient/Output/IOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongClient.Output
+{
+    public interface IOutput
+    {
+        public void Write(string message);
+    }
+}
