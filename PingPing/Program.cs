9ffc8d998431ddc7289f65f04eadf8105f6bae7b
noam79 <noameilon8@gmail.com>
2022-01-19 14:53:33 +0200

    Added empty project

---
+++ PingPing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PingPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
