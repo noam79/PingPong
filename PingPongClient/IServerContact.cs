d0056bae0553b1d4d382b8e758993d4f660196ae
noam79 <noameilon8@gmail.com>
2022-01-19 17:21:20 +0200

    Added IServerContact

---
+++ PingPongClient/IServerContact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongClient
+{
+    public interface IServerContact : IDisposable
+    {
+        public void Connect(string ip, int port);
+
+        public void Send(object obj);
+
+        public object Recieve();
+    }
+}
