872637343cd824151bc62961ceeb6385aa0dd284
noam79 <noameilon8@gmail.com>
2022-01-19 16:57:00 +0200

    Added PingPongSocketHandler

---
+++ PingPongServer/SocketClientHandlers/PingPongSocketHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer.SocketClientHandlers
+{
+    public class PingPongSocketHandler : ISocketClientHandler
+    {
+        private bool _run;
+
+        public void HandleClient(Socket socket)
+        {
+            _run = true;
+
+            while (_run)
+            {
+                string data = null;
+                byte[] buffer = null;
+
+                while (_run)
+                {
+                    buffer = new byte[1024];
+                    int bytesReceived = socket.Receive(buffer);
+                    data += Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                socket.Send(Encoding.ASCII.GetBytes(data));
+            }
+        }
+
+        public void Close()
+        {
+            _run = false;
+        }
+    }
+}
