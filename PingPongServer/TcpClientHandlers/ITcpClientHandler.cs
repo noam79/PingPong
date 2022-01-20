010d3da67e413cd71b3d841085f47a1737922c1b
noam79 <noameilon8@gmail.com>
2022-01-20 09:34:44 +0200

    Added ITcpClientHandler

---
+++ PingPongServer/TcpClientHandlers/ITcpClientHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer.TcpClientHandlers
+{
+    public interface ITcpClientHandler
+    {
+        public void HandleClient(TcpClient socket);
+
+        public void Close();
+    }
+}
